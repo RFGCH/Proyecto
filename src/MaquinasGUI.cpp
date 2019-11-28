dabf4193c8e53540f7b53d4e806d2639d196e5ca
RFGCH <54367628+RFGCH@users.noreply.github.com>
2019-11-28 14:07:05 -0500

    Update MaquinasGUI.cpp

--- src/MaquinasGUI.cpp
+++ src/MaquinasGUI.cpp
@@ -53,6 +53,27 @@ void MaquinasGUI::Vistarapida(){
     cin >> opc;
 }
 void MaquinasGUI::Buscar(){
+    system("cls");
+    ifstream leer("Datos//maquinas.txt");
+    if(leer.eof()){
+        cout << "\n  ***No hay maquinas que mostrar***" << endl;
+    }
+        string opc;
+        cout << "\n ******  Eliminar  ******" << endl;
+        cout << "\n   1.-Estoy seguro de eliminar mi usuario" << endl;
+        cout << "\n   2.-Salir" << endl;
+        cin >> opc;
+    if
+    while(!leer.eof()){
+        string codigo,resumen;
+        getline(leer,codigo);
+        cout << "\n ---- " << codigo << " ---   \n"<< endl;
+        getline(leer,resumen);
+        cout << " " << resumen << "...\n" << endl;
+    }
+    cout << "\n Ingrese cualquier tecla... ";
+    string opc;
+    cin >> opc;
 }
 void MaquinasGUI::Agregar(){
 }
